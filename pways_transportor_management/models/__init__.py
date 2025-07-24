1a1c063f5fdb3f92a3c8317297baadf2dfa325eb
manolo <software@klo.es>
2025-07-24 12:51:21 +0200

    Borramos los módulos de extra que son privativos porque los hemos pasado al repositorio extra_privativo.

--- pways_transportor_management/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sale
-from . import stock_picking
-from . import account_invoice
-from . import res_partner
-from . import picking_transport_info
-from . import picking_route
-from . import transporter_route
