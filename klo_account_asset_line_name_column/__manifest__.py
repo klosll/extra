6ed8b4d6c2fb139f5d0707c24de11e1b752c614d
manolo <software@klo.es>
2025-08-25 13:42:31 +0200

    klo_account_asset_line_name_column: Nueva columna con sólo el Nombre del activo en la lista de Activos->Líneas de activos.

---
+++ klo_account_asset_line_name_column/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 KLO Ingeniería Informática S.L.L. (https://www.klo.es)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Asset Line Name Column",
+    "version": "16.0.0.1.0",
+    "category": "Account",
+    "sequence": 80,
+    "summary": "Column Name of Asset",
+    "license": "AGPL-3",
+    "author": "KLO Ingeniería Informática S.L.L.",
+    "website": "https://www.klo.es",
+    "installable": True,
+    "auto_install": False,
+    "depends": ["account", "account_asset_line_menu"],
+    "data": [
+        "views/account_asset_line_views.xml",
+    ],
+}
