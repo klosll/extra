63c86d86783fd2183c91d1397dce07b4e9dbc767
manolo <software@klo.es>
2025-09-23 12:19:29 +0200

    klo_account_invoice_price_as_percent: En QWeb impreso de Facturas (clientes). Precio unitario * 100 al ser un % lo que se usa, no un precio.

---
+++ klo_account_invoice_price_as_percent/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Price as percent",
+    "version": "16.0.1.0.0",
+    "category": "Account",
+    "license": "AGPL-3",
+    "author": "KLO Ingeniería Informática S.L.L.",
+    "website": "https://www.klo.es",
+    "depends": [
+        "account",
+    ],
+    "excludes": [],
+    "data": [
+        "report/account_invoice_report.xml",
+    ],
+    "installable": True,
+}
