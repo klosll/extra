e4a8a890f71613b06fd5dcf1d8b41e6aa77a8f92
manolo <software@klo.es>
2025-10-09 13:47:52 +0200

    Creación de rama 18.0 se quitan los módulos que viene de la rama 16.0 que es la padre.

--- klo_tare_net_on_picking/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import purchase
-from . import sale_order
-from . import stock_move
-from . import res_company
--- klo_xf_sale_project/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import project
-from . import account_move
