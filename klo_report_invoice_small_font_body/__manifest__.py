5e315633eb79e668eecd9e3ce6d8fcc52e9405c8
manolo <software@klo.es>
2025-12-17 13:14:50 +0100

    klo_report_invoice_small_font_body: Cambia el tamaño de letra en el cuerpo de QWeb de Factura. El tamaño de ñetra se asigna en el Parámetro de sistema klo.report_small_font_body por defecto 8px

---
+++ klo_report_invoice_small_font_body/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# KLO
+{
+    'name': 'KLO - Invoice report small font body',
+    'version': '18.0.0.1',
+    'summary': 'Invoice report body with smaller font size for more compact printing',
+    'description': 'Se auto-crea el Parámetro de sistema: klo.invoice_report_body_small_font con valor 8 por defecto',
+    'category': 'Accounting/Accounting',
+    "license": "AGPL-3",
+    'author': 'KLO Ingenieria Informatica S.L.L.',
+    'website': 'https://www.klo.es',
+    'depends': ['account',],
+    'data': [
+        'data/ir_config_parameter_data.xml',
+        "data/report_paperformat_data.xml",
+        "reports/report_invoice.xml",
+    ],
+    "installable": True,
+    "application" : False,
+}
+
