69241ec9eec7ccb667bc464876c20ca79c179d3e
manolo <software@klo.es>
2026-01-14 11:55:44 +0100

    klo_compact_head_layout: Ahora instala el módulo dependiente klo_report_layout_small_font_header para usar el parámetro klo.report_layout_small_font_header El texto de nuestra empresa lo pone del que pone en la configuración formateado como esté.

--- klo_compact_head_layout/__manifest__.py
+++ klo_compact_head_layout/__manifest__.py
@@ -7,12 +7,14 @@
     'description': """
     Hace la cabecera de los informes en formato QWeb más compacta, mostrando los datos del cliente en la misma 
     línea que el nombre y NIF de la empresa.
+    Se usa el parámetro de sistema 'klo.report_layout_small_font_header' para reducir el tamaño de la fuente de la cabecera.
+    que viene del módulo dependiente 'klo_report_layout_small_font_header'.
     """,
     'category': 'Uncategorized',
     "license": "AGPL-3",
     'author': 'KLO Ingenieria Informatica S.L.L.',
     'website': 'https://www.klo.es',
-    'depends': ['account','web'],
+    'depends': ['account','web',"klo_report_layout_small_font_header"],
     'data': [
         "views/report_templates.xml",
         "views/report_assets.xml",
