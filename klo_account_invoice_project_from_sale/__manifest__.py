4a82b5330fff1acb7becfe4d5c0a708889e9d68e
manolo <software@klo.es>
2026-01-16 12:19:46 +0100

    klo_account_invoice_project_from_sale: Imprimir el proyecto en el QWeb de presupuesto/pedido si lo tiene.

--- klo_account_invoice_project_from_sale/__manifest__.py
+++ klo_account_invoice_project_from_sale/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "KLO Project on Invoice from Origin Sale Order",
+    "name": "KLO Project on Invoice from Origin Sale Order and print on sale order and invoice reports",
     "version": "18.0.1.0.1",
     "author": "KLO Ingeniería Informática S.L.L.",
     "maintainers": ["Manuel Calomarde Gómez"],
@@ -14,6 +14,7 @@
                 'project',
                 ],
     "data": ["views/account_move_view.xml",
+             "reports/sale_order_templates.xml",
              "reports/invoice_report.xml",
              ],
     "installable": True,
