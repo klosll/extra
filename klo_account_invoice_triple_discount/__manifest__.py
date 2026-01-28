04ffd14de95c13899635ab2b7357287a398f6dbe
manolo <software@klo.es>
2026-01-28 09:00:01 +0100

    klo_account_invoice_triple_discount: Mejora sobre el módulo OCA account_invoice_triple_discount Ahora imprime las columnas de descuento si tienen dato. Ya no usa el False/True de display_discount a piñón del módulo original.

---
+++ klo_account_invoice_triple_discount/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 QubiQ (http://www.qubiq.es)
+# Copyright 2017 Tecnativa - David Vidal
+# Copyright 2026 KLO Ingeniería Informática S.L.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "KLO Improvement for Account Invoice Triple Discount",
+    "version": "18.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "QubiQ, Tecnativa, GRAP, Odoo Community Association (OCA), KLO Ingeniería Informática S.L.L.",
+    "website": "https://www.klo.es",
+    "license": "AGPL-3",
+    "summary": "Improvement for OCA Manage triple discount on invoice lines by KLO",
+    "depends": ["account_invoice_triple_discount"],
+    "data": ["report/invoice.xml",],
+    "installable": True,
+}
