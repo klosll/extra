adc6c2bb012a424d39899e7255cc89c5ac31e41d
KLO <software@klo.es>
2026-03-13 13:17:50 +0100

    klo_group_by_bom: Añade la opcion de menu "Lista de materiales" en la lista de Despiece para no tener que buscarlo en Agregar grupo personalizado.

---
+++ klo_group_by_bom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# KLO
+{
+    'name': 'KLO - Agrupar por Lista de Materiales',
+    'version': '14.0.1.0.0',
+    'summary': 'Añade la opción de agrupar por Lista de Materiales en las Órdenes de Fabricación',
+    'author': 'KLO Ingenieria Informatica S.L.L.',
+    'website': 'https://www.klo.es',
+    'category': 'Manufacturing',
+    'depends': ['mrp'],
+    'data': [
+        'views/mrp_production_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
