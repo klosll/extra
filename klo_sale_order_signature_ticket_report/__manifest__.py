1d5ca4a0413ed7b0ded42f098305ec35ac74acf0
KLO <software@klo.es>
2026-06-25 10:48:42 +0200

    klo_sale_order_signature_ticket_report: QWeb de Ticket heredado del ticket normal de pedido pero con la firma del cliente al final.

---
+++ klo_sale_order_signature_ticket_report/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "KLO Sale Order Signature Ticket Report",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "batch_liquidation_report",
+        "sale_order_digitized_signature",
+    ],
+    "data": [
+        "report/sale_order_signature_ticket_report.xml",
+    ],
+    "installable": True,
+}
