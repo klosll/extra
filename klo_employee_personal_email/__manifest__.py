c4f712d1e79b00c3d4d5d9d96130f621c8ea25f6
KLO <software@klo.es>
2026-07-15 14:02:23 +0200

    klo_employee_personal_email: Visualiza el correo-e particular en la lista de Empleados.

---
+++ klo_employee_personal_email/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'KLO - Employee Personal Email',
+    'version': '14.0.1.0.0',
+    'summary': 'Adds Private Email column to Employee tree view for HR Managers',
+    'description': """
+        This module adds the Private Email (private_email) field to the Employee
+        tree view, displayed to the right of the Work Phone column.
+        Only visible to users in the Empleados/Administrador group.
+    """,
+    'category': 'Human Resources',
+    'author': 'KLO',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': ['hr'],
+    'data': [
+        'views/hr_employee_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
