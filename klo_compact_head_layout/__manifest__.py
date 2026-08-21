ec16e229842404c090468bd7d2c4f4684d2024d2
KLO <software@klo.es>
2026-08-21 19:04:38 +0200

    Normaliza metadatos, cabeceras y documentación técnica; crea models/

--- klo_compact_head_layout/__manifest__.py
+++ klo_compact_head_layout/__manifest__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+# Copyright 2026 KLO Ingeniería Informática S.L.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # KLO
 {
     'name': 'KLO - Cabecera QWeb compacta con datos de cliente',
-    'version': '18.0.0.1',
+    'version': '18.0.0.1.0',
     'summary': 'Hace la cabecera de los informes en formato QWeb más compacta',
     'description': """
     Hace la cabecera de los informes en formato QWeb más compacta, mostrando los datos del cliente en la misma 
@@ -21,6 +23,6 @@
         "reports/report_sale_picking_invoice_documents.xml",
     ],
     "installable": True,
-    "application" : False,
+    "auto_install" : False,
 }
 
