94deef8a10bf76526cd38eefdad864e0a4f69c96
KLO <software@klo.es>
2026-08-21 19:04:38 +0200

    Añade cabeceras de licencia a manifest y modelo

--- klo_bank_statement_cascade_delete/__manifest__.py
+++ klo_bank_statement_cascade_delete/__manifest__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright 2026 KLO Ingeniería Informática S.L.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'KLO - Bank Statement Cascade Delete',
     'version': '18.0.1.0.0',
