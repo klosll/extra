15cc20573ebb9c197296d6b4d2c8ceb57b65c62c
KLO <software@klo.es>
2026-08-21 19:04:38 +0200

    Normaliza metadatos, cabeceras y documentación técnica

--- klo_po_supplierinfo_triple_discount/__manifest__.py
+++ klo_po_supplierinfo_triple_discount/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2026 KLO
+# Copyright 2026 KLO Ingeniería Informática S.L.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "KLO - PO Supplierinfo Triple Discount Update",
@@ -16,7 +16,6 @@
         "purchase_triple_discount",
     ],
     "installable": True,
-    "application": False,
     "auto_install": False,
 }
 
--- klo_po_supplierinfo_triple_discount/models/purchase_order_line.py
+++ klo_po_supplierinfo_triple_discount/models/purchase_order_line.py
@@ -1,4 +1,4 @@
-# Copyright 2026 KLO
+# Copyright 2026 KLO Ingeniería Informática S.L.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import models
