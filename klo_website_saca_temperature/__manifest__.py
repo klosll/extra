bbd524408529f06acb9f9a9030c57cc6ebbf2e14
KLO <software@klo.es>
2026-09-01 17:27:20 +0200

    klo_website_saca_temperature: Añade el campo de temperatura en la vista de líneas de SACA en el formulario del portal de SACA.

---
+++ klo_website_saca_temperature/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 KLO Ingeniería Informática
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "KLO Website Saca Temperature",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "license": "AGPL-3",
+    "author": "KLO Ingeniería Informática",
+    "depends": [
+        "website_custom_saca",
+        "klo_saca_temperature",
+    ],
+    "data": [
+        "views/portal_saca_line_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ klo_website_saca_temperature/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2026 KLO Ingeniería Informática
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+# No new models needed, just inheriting views
