c910a47ca10ff2641fc712f44cb026de7b99b65c
KLO <software@klo.es>
2026-09-04 13:06:44 +0200

    klo_account_invoice_triple_discount: Actualizar manifest para reflejar el cambio en el nombre y resumen del módulo.

--- klo_account_invoice_triple_discount/__manifest__.py
+++ klo_account_invoice_triple_discount/__manifest__.py
@@ -3,13 +3,13 @@
 # Copyright 2026 KLO Ingeniería Informática S.L.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "KLO Improvement for Account Invoice Triple Discount",
+    "name": "KLO Improvement for Account Invoice Triple Discount. Show only non-zero dto column",
     "version": "18.0.1.0.0",
     "category": "Accounting & Finance",
     "author": "QubiQ, Tecnativa, GRAP, Odoo Community Association (OCA), KLO Ingeniería Informática S.L.L.",
     "website": "https://www.klo.es",
     "license": "AGPL-3",
-    "summary": "Improvement for OCA Manage triple discount on invoice lines by KLO",
+    "summary": "Only show dto column with value <> 0. Improvement for OCA Manage triple discount on invoice lines by KLO.",
     "depends": ["account_invoice_triple_discount"],
     "data": ["report/invoice.xml",],
     "installable": True,
