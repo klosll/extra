141a0978f66e6cf612dfbc0d79519ad78db89d48
KLO <software@klo.es>
2026-09-21 12:54:46 +0200

    klo_invoice_report_grouped_by_sale_order_commitment_date: Se pone en QWeb el campo de Fecha de entrega del pedido de venta en la agrupación por pedido de venta

---
+++ klo_invoice_report_grouped_by_sale_order_commitment_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 KLO Ingeniería Informática S.L.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "KLO - Invoice Report Grouped by Sale Order with commitment date",
+    "version": "18.0.1.0.2",
+    "summary": "Imprime facturas con líneas agrupadas por pedido de venta con fecha de entrega ordenadas de fecha antigua a reciente",
+    "license": "AGPL-3",
+    "author": "KLO Ingenieria Informatica S.L.L.",
+    "website": "https://www.klo.es",
+    "category": "Accounting & Finance",
+    "depends": ["account", "sale"],
+    "data": ["views/report_invoice.xml"],
+    "installable": True,
+    "auto_install": False,
+}
